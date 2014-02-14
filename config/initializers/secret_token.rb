6f337196cbe2f024eb1b29d8adbf4372e0e694f6
jslive <jslivebc@hotmail.com>
2014-02-15 09:09:35 +1100

    initialise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'd71ca7d6bd67a2e51a2618706c53e4088c85822a613924e21b0c2960c5bc493d5e7513a1157c3041bf5854f8c60bf472e1c6fd317f6fc9b63490e2efb6b6a3d1'
